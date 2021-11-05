3a74d0a1cafff5e98c6cfeb2eb5d068602a9bf46
Fabian Jochen Kanzler <hello@fabianjochenkanzler>
2021-11-05 14:17:55 +0100

    3.1 serve CSS

--- myApp.js
+++ myApp.js
@@ -4,9 +4,12 @@ var app = express();
 console.log("Hello World");
 
 const indexPath = __dirname + "/views/index.html";
+const cssPath = __dirname + "/public/style.css";
 
 app.get("/", function (req, res) {
   res.sendFile(indexPath);
 });
+
+app.get("/public", express.static(cssPath));
 
 module.exports = app;
